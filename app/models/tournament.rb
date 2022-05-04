0ded4b0c32c038f1c7689ac267f02687eed94f80
ikael <ikael@student.21-school.ru>
2022-05-04 03:40:28 +0300

    Tournament
    one tournament can have many leagues

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,12 @@
+# Base class
+# See *_tournament.rb files in app/models directory
+# Warning!!!
+# app/models/league_tournament.rb file does not inherit from Tournament
+# It is needed to associate multiple tournaments with multiple leagues
 class Tournament < ActiveRecord::Base
+  has_and_belongs_to_many :leagues
+
   validates :name, presence: true
+  validates :name, uniqueness: true
   validates :name, length: { minimum: 2 }
 end
