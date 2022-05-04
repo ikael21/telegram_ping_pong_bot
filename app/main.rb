311227a4099cfffc42bf2f040973ba993fca1338
ikael <ikael@student.21-school.ru>
2022-05-04 03:42:57 +0300

    main.rb
    moved require part to app_requirements and model_requirements

--- app/main.rb
+++ app/main.rb
@@ -1,16 +1,4 @@
-require 'telegram/bot'
-require 'active_record'
-require_relative '../bot_token'
-
-
-def configure_db
-  db_config_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
-  YAML.load(File.read(db_config_file))
-end
-
-
-ActiveRecord::Base.establish_connection(configure_db['development'])
-
+require_relative 'app_requirements'
 
 Telegram::Bot::Client.run(BOT_TOKEN) do |bot|
   bot.listen do |message|
