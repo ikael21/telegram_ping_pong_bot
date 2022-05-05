e4811ccab65c812f90a9e21d11c37ef578a0fd19
ikael <ikael@student.21-school.ru>
2022-05-05 14:23:46 +0300

    Tournament model
    deleted comments
    added validations for new columns

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,12 +1,12 @@
-# Base class
-# See *_tournament.rb files in app/models directory
-# Warning!!!
-# app/models/league_tournament.rb file does not inherit from Tournament
-# It is needed to associate multiple tournaments with multiple leagues
 class Tournament < ActiveRecord::Base
   has_and_belongs_to_many :leagues
 
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { minimum: 2 }
+  validates :organizer, presence: true
+  validates :status, presence: true
+
+  enum status: [:registration, :playing, :finished]
+  validates :status, inclusion: { in: statuses.keys }
 end
