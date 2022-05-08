3ffc73be1b0f60e918fbb56e7aedf4da8594bcfa
ikael <ikael@student.21-school.ru>
2022-05-08 21:29:44 +0300

    app requirements
    setup for correct postgres usage

--- app/app_requirements.rb
+++ app/app_requirements.rb
@@ -3,11 +3,14 @@ require 'active_record'
 require_relative '../bot_token'
 require_relative 'model_requirements'
 require_relative 'app'
+require 'erb'
 
 def configure_db
-  db_config_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
-  YAML.load(File.read(db_config_file))
+  db_config_file = File.join(
+    File.expand_path('..', __FILE__), '..', 'db', 'database.yml'
+  )
+  YAML.load(ERB.new(File.read(db_config_file)).result)
 end
 
-# connect to sqlite3
+# connect to a database (PostgreSQL)
 ActiveRecord::Base.establish_connection(configure_db['development'])
