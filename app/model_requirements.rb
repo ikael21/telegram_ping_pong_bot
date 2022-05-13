75ff58760e1f7584b60dd0788af388ca87cdae5a
ikael <ikael@student.21-school.ru>
2022-05-13 10:55:56 +0300

    model requirements
    first requited user.rb
    then the rest files inside app/models

--- app/model_requirements.rb
+++ app/model_requirements.rb
@@ -1,4 +1,4 @@
-require File.expand_path 'app/models/tournament.rb'
+require File.expand_path 'app/models/user.rb'
 Dir['app/models/*.rb'].each do |file|
-  require File.expand_path(file) unless file == 'app/models/tournament.rb'
+  require File.expand_path(file)
 end
