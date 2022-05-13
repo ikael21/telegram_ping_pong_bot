48df76be3cd27ff4ca839dc265ed9e4b6b96608f
ikael <ikael@student.21-school.ru>
2022-05-13 10:57:53 +0300

    Seeds
    added fake data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,34 @@ leagues.each do |league|
     tournament.save
   end
 end
+
+PLAYERS = [
+  {
+    first_name: 'Anton',
+    last_name: 'Yudin',
+    nickname: 'ikael'
+  },
+  {
+    first_name: 'Ant',
+    last_name: 'Yud',
+    nickname: 'fess'
+  },
+  {
+    first_name: 'Antonio',
+    last_name: 'Vivaldi',
+    nickname: 'kar'
+  },
+  {
+    first_name: 'Ivette',
+    last_name: 'Kael',
+    nickname: 'laeda'
+  }
+]
+
+PLAYERS.each do |player|
+  Player.create(
+    first_name: player[:first_name],
+    last_name: player[:last_name],
+    nickname: player[:nickname]
+  )
+end
