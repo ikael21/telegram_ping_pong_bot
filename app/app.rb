3a8bba768acad77a59e7ed62da9f3c4e82e3c3cd
ikael <ikael@student.21-school.ru>
2022-05-15 12:44:57 +0300

    App
    For now, all logic with telegram bot is in app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,32 +1,40 @@
+# frozen_string_literal: true
+
+require_relative '../config/app_config'
+
+# class represents bot app
 class App
   class << self
-
     def handle_greetings(bot, message)
       reply_keyboard = Telegram::Bot::Types::ReplyKeyboardMarkup.new(
-        keyboard: [ %w(REGISTER) ],
+        keyboard: [['REGISTER']],
         one_time_keyboard: true,
-        resize_keyboard: true
+        resize_keyboard: true,
       )
       bot.api.send_message(
         chat_id: message.chat.id,
         text: "Hello #{message.from.first_name}!",
-        reply_markup: reply_keyboard
+        reply_markup: reply_keyboard,
       )
     end
 
-    # OPTIMIZE change this method
+    # OPTIMIZE: change this method
     def handle_message(bot, message)
-      p message
       case message
       when Telegram::Bot::Types::Message
-        if message.text == '/start'
-          handle_greetings(bot, message)
-        else
-          bot.api.send_message(chat_id: message.chat.id, text: "Not implemented")
-        end
+        handle_greetings(bot, message) if message.text == '/start'
       else
-        bot.api.send_message(chat_id: message.chat.id, text: "Not implemented")
+        bot.api.send_message(
+          chat_id: message.chat.id,
+          text: 'Not implemented',
+        )
       end
     end
+  end
+end
+
+Telegram::Bot::Client.run(BOT_TOKEN) do |bot|
+  bot.listen do |message|
+    App.handle_message(bot, message)
   end
 end
