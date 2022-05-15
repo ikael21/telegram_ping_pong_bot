4991ea0cc498ffd2027da222e4ee70b18e3ac350
ikael <ikael@student.21-school.ru>
2022-05-15 21:03:36 +0300

    Tests
    writen tests for League model

--- test/model/league_test.rb
+++ test/model/league_test.rb
@@ -3,8 +3,18 @@
 require 'spec_helper'
 
 RSpec.describe(League) do
-  it 'saves itself inside a database' do
-    league = FactoryBot.create(:league)
-    expect(league.save).to(eq(true))
+  subject { described_class.new(name: 'league') }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to(be_valid)
+  end
+
+  it 'is not valid without attributes' do
+    expect(League.new).to(be_invalid)
+  end
+
+  it 'is not valid without name' do
+    subject.name = nil
+    expect(subject).to(be_invalid)
   end
 end
