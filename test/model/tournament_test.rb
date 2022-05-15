7e3999dd0836d96ca842eb961529ed2a6660e37a
ikael <ikael@student.21-school.ru>
2022-05-15 21:04:10 +0300

    Tournament Tests
    writen tests for Tournament model

--- test/model/tournament_test.rb
+++ test/model/tournament_test.rb
@@ -3,8 +3,34 @@
 require 'spec_helper'
 
 RSpec.describe(Tournament) do
-  it 'saves itself inside a database' do
-    tournament = FactoryBot.create(:tournament)
-    expect(tournament.save).to(eq(true))
+  subject {
+    described_class.new(
+      name: 'tournament_name',
+      organizer: 'orginizer',
+      status: 'registration'
+    )
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to(be_valid)
+  end
+
+  it 'is not valid without attributes' do
+    expect(Tournament.new).to(be_invalid)
+  end
+
+  it 'is not valid without name' do
+    subject.name = nil
+    expect(subject).to(be_invalid)
+  end
+
+  it 'is not valid without organizer' do
+    subject.organizer = nil
+    expect(subject).to(be_invalid)
+  end
+
+  it 'is not valid without status' do
+    subject.status = nil
+    expect(subject).to(be_invalid)
   end
 end
