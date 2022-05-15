9ed5422700f6cf2971d3ae334b09220adb0d108b
ikael <ikael@student.21-school.ru>
2022-05-15 14:10:02 +0300

    Tests for Player model
    code refactoring

--- test/model/player_test.rb
+++ test/model/player_test.rb
@@ -3,40 +3,38 @@
 require 'spec_helper'
 
 RSpec.describe(Player) do
+  subject {
+    described_class.new(
+      first_name: 'first_name',
+      last_name: 'last_name',
+      nickname: 'nickname'
+    )
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to(be_valid)
+  end
+
   it 'is not valid without attributes' do
     expect(Player.new).to(be_invalid)
   end
 
   it 'is not valid without first name' do
-    player = Player.new do |p|
-      p.last_name = 'last_name'
-      p.nickname = 'nickname'
-    end
-    expect(player).to(be_invalid)
+    subject.first_name = nil
+    expect(subject).to(be_invalid)
   end
 
   it 'is not valid without last name' do
-    player = Player.new do |p|
-      p.first_name = 'first_name'
-      p.nickname = 'nickname'
-    end
-    expect(player).to(be_invalid)
+    subject.last_name = nil
+    expect(subject).to(be_invalid)
   end
 
   it 'is not valid without nickname' do
-    player = Player.new do |p|
-      p.first_name = 'first_name'
-      p.last_name = 'last_name'
-    end
-    expect(player).to(be_invalid)
-  end
-
-  it 'is valid with valid attributes' do
-    player = FactoryBot.create(:player)
-    expect(player).to(be_valid)
+    subject.nickname = nil
+    expect(subject).to(be_invalid)
   end
 
   it 'has type `Player`' do
-    expect(Player.new.type).to(eq('Player'))
+    expect(subject.type).to(eq('Player'))
   end
 end
