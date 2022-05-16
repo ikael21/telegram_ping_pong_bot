8f6d9abfcf50342fbaab00417ab64d4971653c5a
ikael <ikael@student.21-school.ru>
2022-05-16 18:00:19 +0300

    Player model
    Now all validations from User model are here

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-class Player < User
+class Player < ActiveRecord::Base
+  has_and_belongs_to_many :teams
+
+  validates_presence_of :nickname
+  validates :nickname, uniqueness: true
 end
