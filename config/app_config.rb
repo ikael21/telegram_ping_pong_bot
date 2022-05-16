1d35ddb7f3e99371dd19a0fce06ca73bc319be4f
ikael <ikael@student.21-school.ru>
2022-05-16 18:08:59 +0300

    App config
    added else block for case-when if no RAILS_ENV given or it's invalid

--- config/app_config.rb
+++ config/app_config.rb
@@ -4,14 +4,15 @@ require 'telegram/bot'
 require 'active_record'
 require_relative '../bot_token'
 require_relative 'model_requirements'
+require_relative '../app/app'
 require 'erb'
 
 # load database config file into memory
 def configure_db
-  db_config_file = File.join(
+  config = File.join(
     File.expand_path(__dir__), '..', 'db', 'config.yml'
   )
-  YAML.load(ERB.new(File.read(db_config_file)).result)
+  YAML.load(ERB.new(File.read(config)).result)
 end
 
 # connect to a database depending on the environment
@@ -20,4 +21,7 @@ when 'development'
   require_relative './environments/development'
 when 'test'
   require_relative 'environments/test'
+else
+  puts 'RAILS_ENV is not provided or invalid'
+  exit 1
 end
