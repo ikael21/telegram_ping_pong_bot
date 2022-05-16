ec5f25a62f84b934538834add8907ae39993bfff
ikael <ikael@student.21-school.ru>
2022-05-16 18:07:42 +0300

    Team model
    has many and belongs to players
    name attribute is unique

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Team < ActiveRecord::Base
+  has_and_belongs_to_many :players
+
   validates_presence_of :name
+  validates :name, uniqueness: true
   validates :name, length: { minimum: 2 }
 end
