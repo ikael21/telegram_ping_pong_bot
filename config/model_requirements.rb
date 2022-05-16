40c22ed23df6cf075fbaf34e432fd53cf43e7e01
ikael <ikael@student.21-school.ru>
2022-05-16 17:59:31 +0300

    Model requirements
    deleted seperate require for User model
    as from now there's no users

--- config/model_requirements.rb
+++ config/model_requirements.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # This file requires all the model classes from app/models
-require File.expand_path 'app/models/user.rb'
 Dir['app/models/*.rb'].each do |file|
   require File.expand_path(file)
 end
