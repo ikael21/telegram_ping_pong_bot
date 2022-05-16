5c505f4abd25fad5db5a0e36147e505f2ca9cb3b
ikael <ikael@student.21-school.ru>
2022-05-16 18:01:49 +0300

    User model
    no user model from now, only players

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class User < ActiveRecord::Base
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :nickname
-  validates :nickname, uniqueness: true
-end
